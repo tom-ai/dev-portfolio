7b80f0789c469dcc9f1219aed06361a152948412
Tom Harris <tomharris3@me.com>
2023-10-23 13:30:31 +0100

    add test and return to .js

--- src/app/utils/makeImageURL.test.ts
+++ src/app/utils/makeImageURL.test.ts
@@ -1,17 +1,17 @@
-// import { makeImageURL } from './makeImageURL';
+import { makeImageURL } from './makeImageURL';
 
-// describe('makeImageURL', () => {
-//   it('returns null if domain is undefined', () => {
-//     // something
-//     const url = makeImageURL('/uploads/test.jpg', undefined);
+describe('makeImageURL', () => {
+  it('returns null if domain is undefined', () => {
+    // something
+    const url = makeImageURL('/uploads/test.jpg', undefined);
 
-//     expect(url).toBeNull();
-//   });
+    expect(url).toBeNull();
+  });
 
-//   //   it('should return a URL with Strapi domain at the beginning', () => {
-//   //     const strapiDomain = 'http://127.1.2.3:5555';
-//   //     const url = makeImageURL('/uploads/test.jpg', strapiDomain);
+  //   it('should return a URL with Strapi domain at the beginning', () => {
+  //     const strapiDomain = 'http://127.1.2.3:5555';
+  //     const url = makeImageURL('/uploads/test.jpg', strapiDomain);
 
-//   //     expect(url).toBe('http://127.1.2.3:5555/uploads/test.jpg');
-//   //   });
-// });
+  //     expect(url).toBe('http://127.1.2.3:5555/uploads/test.jpg');
+  //   });
+});
